1eb9cc771f2eabbec5617f07ad17f7055c032158
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-12-12 14:16:12 -0300

    Incluindo o arquivo README

--- src/br/com/aodes/desafiodecodigo/introducaoprogramacaojava/VisitaNaFeira.java
+++
@@ -1,7 +0,0 @@
-package br.com.aodes.desafiodecodigo.introducaoprogramacaojava;
-
-public class VisitaNaFeira {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/br/com/aodes/desafiodecodigo/introducaoprogramacaojava/visitanafeira/README.md
@@ -0,0 +1,2 @@
+# digital-innovation-one
+ Repositório de desafios da plataforma Digital Innovation One
